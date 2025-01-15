e811d19c342f5de6f4c90a776fb532aed5a54847
ngocanh235 <phamngocanh230505@gmail.com>
2025-01-16 01:44:30 +0700

    he

--- quanlyphong/thuephong.php
+++ quanlyphong/thuephong.php
@@ -55,6 +55,7 @@ $room_info = $conn->query("SELECT room_name, address_room, price FROM rooms WHER
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Thuê Phòng</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="/baiTapLon/quanlyphong/thuephong.css">
 </head>
 <body>
