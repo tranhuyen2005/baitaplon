fa462991a42810e07064094777809c924bec00e4
NguyenThiYenNhi23 <yennhi080923@gmail.com>
2025-01-15 23:11:32 +0700

    12

--- quanlyphong/sua.php
+++ quanlyphong/sua.php
@@ -68,7 +68,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sửa Thông Tin Phòng</title>
-
+    <link rel="stylesheet"href="sua.css">
 </head>
 <body>
     <div class="container mt-4">
